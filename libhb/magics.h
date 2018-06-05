5d4f713a4bb8f0cbcc681866d6753b1a462bb9cf
ExodusSecurity <30537108+ExodusSecurity@users.noreply.github.com>
2018-06-05 16:09:35 +0100

    update magics.h

--- libhb/magics.h
+++ libhb/magics.h
@@ -96,6 +96,7 @@
 #define __copyin					     			                    0x286DF0
 #define __copyout					     			                    0x286D70
 #define __copyinstr                                     0x287160
+#define	__UART_ENABLE 	                                0x186b0a0
 #define __copystr                                       0x2871F0
 #define __printf			     					                    0x347580
 #define __device_printf                                 0x3FADB0
@@ -294,6 +295,7 @@
 #define __copyin                                        0x14A890
 #define __copyout                                       0x14A7B0
 #define __copyinstr                                     0x14AD00
+#define	__UART_ENABLE 	                                0x1997BC8
 
 #define __copystr                                       0x2871F0
 #define __strlen     								                    0x1D3640
@@ -426,32 +428,35 @@
 #define __dmpdpi                                        0x22CB4E8
 #define __sceSblSsDecryptSealedKey                      0x61F2A0
 #define __getSealedKeySecret                            0x61F420
+#define	__UART_ENABLE 	                                0		// offset is 0 due to mira have uart auto enabled in autorun patches.
 
 #elif defined FW_505
 
-#define __Xfast_syscall                                 0x1C0
+#define __Xfast_syscall                                 0x00001C0
 #define __prison0                                       0x10986a0
 #define __rootvnode                                     0x22C1A70
 #define __pml4pml4i                                     0x22CB560 // Pending verification
 #define __dmpml4i                                       0x22CB564
 #define __dmpdpi                                        0x22CB568
-#define __printf                                        0x436040
-#define __copyin                                        0x1EA710
-#define __copyout                                       0x1EA630
-#define __copyinstr                                     0x1EAB40
-#define __kmem_alloc_contig                             0xF1C90
-#define __kmem_free                                     0xFCE50
-#define __pmap_extract                                  0x2E0570
-#define __pmap_protect                                  0x2E3090
-#define __sched_pin                                     0x31FF40
-#define __sched_unpin                                   0x31FF50
-#define __smp_rendezvous                                0x1B85B0
-#define __smp_no_rendevous_barrier                      0x1B8366
-#define __icc_query_nowait                              0x44020
+#define __printf                                        0x0436040
+#define __copyin                                        0x01EA710
+#define __copyout                                       0x01EA630
+#define __copyinstr                                     0x01EAB40
+#define __kmem_alloc_contig                             0x00F1C90
+#define __kmem_free                                     0x00FCE50
+#define __pmap_extract                                  0x02E0570
+#define __pmap_protect                                  0x02E3090
+#define __sched_pin                                     0x031FF40
+#define __sched_unpin                                   0x031FF50
+#define __smp_rendezvous                                0x01B85B0
+#define __smp_no_rendevous_barrier                      0x01B8366
+#define __icc_query_nowait                              0x0044020
 #define __kernel_map                                    0x1AC60E0
 #define __sysent                                        0x107C610
 #define __kernel_pmap_store                             0x22CB570
 #define __Starsha_UcodeInfo                             0
 #define __gpu_devid_is_9924                             0x4DE010
 #define __gc_get_fw_info                                0x4D37A0
+#define	__UART_ENABLE 	                                0		// offset is 0 due to mira have uart auto enabled in autorun patches.
+
 #endif
